533eefe2b63136411671775bf5262f6ac2d7d8de
unknown <rifqizidane1999@gmail.com>
2021-10-25 21:51:16 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuan4_recyclerview/Mahasiswa.java
@@ -0,0 +1,29 @@
+package com.example.pertemuan4_recyclerview;
+
+public class Mahasiswa {
+    private String nama,nim,angkatan;
+    public Mahasiswa(String nama, String nim, String angkatan){
+        this.nama = nama;
+        this.nim = nim;
+        this.angkatan = angkatan;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getNim(){
+        return nim;
+    }
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+    public String getAngkatan(){
+        return angkatan;
+    }
+    public void setAngkatan(String angkatan){
+        this.angkatan = angkatan;
+    }
+}
---
+++ app/src/main/java/com/example/pertemuan4_recyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.pertemuan4_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addData();
+        recyclerView =
+                (RecyclerView)findViewById(R.id.recyclerview);
+        adapter = new MahasiswaAdapter(mahasiswaArrayList);
+        RecyclerView.LayoutManager layoutManager = new
+                LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+    private void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Agung", "1218019",
+                "2012"));
+        mahasiswaArrayList.add(new Mahasiswa("Abi", "1118002",
+                "2011"));
+        mahasiswaArrayList.add(new Mahasiswa("Yudis", "1118031",
+                "2011"));
+    }
+}
